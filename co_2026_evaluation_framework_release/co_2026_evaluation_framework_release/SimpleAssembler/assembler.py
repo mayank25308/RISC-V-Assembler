251a0d9a6bd5f5754007a82db52907436c10638e
mayank25308 <mayank25308@iiitd.ac.in>
2026-03-08 21:42:19 +0530

    intentation error fixed and outputs are also working, checked

--- co_2026_evaluation_framework_release/co_2026_evaluation_framework_release/SimpleAssembler/assembler.py
+++ co_2026_evaluation_framework_release/co_2026_evaluation_framework_release/SimpleAssembler/assembler.py
@@ -1,17 +1,17 @@
 
 registers={"x0":"00000","x1":"00001","x2":"00010","x3":"00011","x4":"00100",
 "x5":"00101", "x6":"00110", "x7":"00111","x8":"01000","x9":"01001",
-"x10":"01010","x11":"01011", "x12":"01100","x13":"01101","x14":"01110",
-"x15":"01111", "x16":"10000","x17":"10001","x18":"10010","x19":"10011",
-"x20":"10100","x21":"10101", "x22":"10110","x23":"10111","x24":"11000",
-"x25":"11001","x26":"11010","x27":"11011", "x28":"11100","x29":"11101",
-"x30":"11110","x31":"11111","zero":"00000","ra":"00001",
-"sp":"00010","gp":"00011", "tp":"00100","t0":"00101", "t1":"00110",
+"x10":"01010", "x11":"01011", "x12":"01100","x13":"01101","x14":"01110",
+"x15":"01111", "x16":"10000", "x17":"10001", "x18":"10010","x19":"10011",
+"x20":"10100", "x21":"10101", "x22":"10110","x23":"10111","x24":"11000",
+"x25":"11001", "x26":"11010", "x27":"11011", "x28":"11100","x29":"11101",
+"x30":"11110", "x31":"11111", "zero":"00000","ra":"00001",
+"sp":"00010", "gp":"00011", "tp":"00100","t0":"00101", "t1":"00110",
 "t2":"00111", "s0":"01000", "fp":"01000", "s1":"01001","a0":"01010",
-"a1":"01011","a2":"01100","a3":"01101","a4":"01110","a5":"01111",
-"a6":"10000","a7":"10001","s2":"10010","s3":"10011","s4":"10100",
-"s5":"10101","s6":"10110","s7":"10111","s8":"11000","s9":"11001",
-"s10":"11010","s11":"11011","t3":"11100","t4":"11101","t5":"11110",
+"a1":"01011", "a2":"01100","a3":"01101","a4":"01110","a5":"01111",
+"a6":"10000", "a7":"10001","s2":"10010","s3":"10011","s4":"10100",
+"s5":"10101", "s6":"10110","s7":"10111","s8":"11000","s9":"11001",
+"s10":"11010", "s11":"11011","t3":"11100","t4":"11101","t5":"11110",
 "t6":"11111"
             }
 
@@ -22,9 +22,9 @@ Rtype={"add":{"opcode":"0110011","func3":"000","func7":"0000000"},
 "sll":{"opcode":"0110011","func3":"001", "func7":"0000000"},
 "slt":{"opcode":"0110011", "func3":"010","func7":"0000000"},
 "sltu":{"opcode":"0110011", "func3":"011","func7":"0000000"},
-"xor":{"opcode":"0110011","func3":"100", "func7":"0000000"},
+"xor":{"opcode":"0110011", "func3":"100", "func7":"0000000"},
 "srl":{"opcode":"0110011", "func3":"101", "func7":"0000000"},
-"or":{"opcode":"0110011","func3":"110","func7":"0000000"},
+"or":{"opcode":"0110011", "func3":"110","func7":"0000000"},
 "and":{"opcode":"0110011", "func3":"111", "func7":"0000000"}}
 
 Itype={"addi":{"opcode":"0010011","func3":"000"},
@@ -151,7 +151,6 @@ def encode_i(parts,label_map,addr):
     if len(parts) != 4:
         print("Wrong number of Arguments")
         exit()
-
     instr = parts[0]
 
     if instr not in Itype :
@@ -196,7 +195,6 @@ def encode_i(parts,label_map,addr):
 
         if parts[2] not in registers:
             print("Wrong Register" , parts[2])
-
             exit()
         
         
@@ -375,7 +373,7 @@ def gen_machine_code(c_line,label_map):
             s_last = False
         else:
             result = None
-           print( "unknonwn instruction", inst)
+            print( "unknonwn instruction", inst)
             s_last = False
         
         addr+=4
@@ -389,7 +387,7 @@ def gen_machine_code(c_line,label_map):
         print("Error: virtual halt is not the last instruction ")
         exit()
 
-    return b_output, error_list
+    return b_output
 
 
 import sys
@@ -399,7 +397,7 @@ output_file= sys.argv[2]
 
 lines = read_file(file)
 label_map, c_line = labelmap_and_lines(lines)
-b_output, error_list =gen_machine_code(c_line,label_map)
+b_output =gen_machine_code(c_line,label_map)
 
 with open(output_file, 'w') as f:
     for b in b_output:
